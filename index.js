d6578759cef3d1f4672496b13a73e13914c97121
hacker-hu-bkl <iamblademaster123@gmail.com>
2024-12-14 01:45:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ import { connectDB, User } from "./db.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*', // or a specific array of domains
+  methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
+  allowedHeaders: 'Content-Type,Authorization', // Allowed headers
+}));
+
 
 dotenv.config({
   path: "./.env",
